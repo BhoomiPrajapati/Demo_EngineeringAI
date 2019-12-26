84a60be326237de284559275dce601ac1ed92844
Bhoomi <bhoomi.prajapati@internal.mail>
2019-12-26 16:18:22 +0530

    Cell added

---
+++ EngineeringAI_Test1/demo_collectionView/demo_collectionView/ViewController/ImageListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ImageListViewController.swift
+//  demo_collectionView
+//
+//  Created by PCQ186 on 26/12/19.
+//  Copyright © 2019 PCQ186. All rights reserved.
+//
+
+import UIKit
+
+final class ImageListViewController: UIViewController {
+
+    // MARK: - IBoutlet
+    
+    // MARK: - Variables
+    
+    // MARK: - Lifecycle methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    // MARK: - Setup
+    private func setup() {
+        
+    }
+    
+    // MARK: - API call
+    
+    // MARK: - Helper methods
+
+}
+
+// MARK: - Extension
+
