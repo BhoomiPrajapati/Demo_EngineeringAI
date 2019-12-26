47f1b1a95f068cc0421078f79f90fd7d0e778016
Bhoomi <bhoomi.prajapati@internal.mail>
2019-12-26 11:16:44 +0530

    Inital commit

---
+++ EngineeringAI_Test1/Demo_List/Demo_List/Networking/APIManager.swift
@@ -0,0 +1,9 @@
+//
+//  APIManager.swift
+//  Demo_List
+//
+//  Created by PCQ186 on 26/12/19.
+//  Copyright © 2019 PCQ186. All rights reserved.
+//
+
+import Foundation
---
+++ EngineeringAI_Test1/Demo_List/Demo_List/TableViewCells/ListDataTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListDataTableViewCell.swift
+//  Demo_List
+//
+//  Created by PCQ186 on 26/12/19.
+//  Copyright © 2019 PCQ186. All rights reserved.
+//
+
+import UIKit
+
+class ListDataTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
